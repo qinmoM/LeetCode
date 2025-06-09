2b6ed869565909983c87dfb579e6b5cfb04fe35d
qinmoM <ma200511@qq.com>
2025-06-09 13:49:07 +0800

    palindrome-number

---
+++ 9.palindrome-number.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+        {
+            return false;
+        }
+        int count = 1;
+        int temp = x;
+        while (temp /= 10)
+        {
+            count++;
+        }
+        for (temp = count; temp > 1 && temp > count / 2; temp--)
+        {
+            int left = x, right = x;
+            for (int a = count - temp; a > 0; a--)
+            {
+                right /= 10;
+            }
+            right %= 10;
+            for (int b = 0; b < temp - 1 ; b++)
+            {
+                left /= 10;
+            }
+            left %= 10;
+            if (left != right)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# LeetCode C++ Solutions  
+  
+> This respository contains my C++ solutions to LeetCode problems.  
+> It serves as a learning record and reference for practcing data structures and algorithms.  
+  
+---
+  
+## Progress  
+  
+_Last updata: 2025-06-09_  
+  
+---
+  
+> Welcome to discuss.  
