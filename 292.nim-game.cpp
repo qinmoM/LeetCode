8385abbc60ba5bd32e361838236fd3ae505fe22a
qinmoM <ma200511@qq.com>
2025-06-30 11:38:16 +0800

    nim-game

---
+++ 292.nim-game.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=292 lang=cpp
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool canWinNim(int n)
+    {
+        return n % 4;
+    }
+};
+// @lc code=end
+
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   
 ## Progress  
   
-_Last updata: 2025-06-29_  
+_Last updata: 2025-06-30_  
   
 ---
   
