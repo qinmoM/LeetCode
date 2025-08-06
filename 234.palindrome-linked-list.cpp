c32d4fec7865a765e45673d743efdfa732676af9
qinmoM <ma200511@qq.com>
2025-08-06 19:56:52 +0800

    list opmitize : palindrome-linked-list

--- 234.palindrome-linked-list.cpp
+++ 234.palindrome-linked-list.cpp
@@ -15,8 +15,6 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <stack>
-
 // struct ListNode
 // {
 //     int val;
@@ -35,47 +33,91 @@ public:
         ListNode* curr = head;
         while (curr)
         {
-            curr = curr->next;
             ++size;
+            curr = curr->next;
         }
         if (0 == size || 1 == size)
         {
             return true;
         }
         curr = head;
-        std::stack<int> s;
-        bool isOddCenter = size & 1;
-        for (int i = 0; i < size; ++i)
+
+        ListNode* newHead = new ListNode(0, head);
+        int gap = (size + 1) / 2 - 1;
+        while (gap)
         {
-            if (size / 2 > i)
+            curr = curr->next;
+            --gap;
+        }
+        while (curr->next)
+        {
+            ListNode* temp = curr->next;
+            curr->next = temp->next;
+            temp->next = newHead->next;
+            newHead->next = temp;
+        }
+        curr = newHead->next;
+        gap = size / 2;
+        while (gap)
+        {
+            if (curr->val == head->val)
             {
-                s.push(curr->val);
+                head = head->next;
                 curr = curr->next;
             }
             else
             {
-                if (s.empty())
-                {
-                    return false;
-                }
-                if (isOddCenter)
-                {
-                    curr = curr->next;
-                    isOddCenter = false;
-                    continue;
-                }
-                if (s.top() == curr->val)
-                {
-                    curr = curr->next;
-                    s.pop();
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
+            --gap;
         }
         return true;
+
+        // int size = 0;
+        // ListNode* curr = head;
+        // while (curr)
+        // {
+        //     curr = curr->next;
+        //     ++size;
+        // }
+        // if (0 == size || 1 == size)
+        // {
+        //     return true;
+        // }
+        // curr = head;
+        // std::stack<int> s;
+        // bool isOddCenter = size & 1;
+        // for (int i = 0; i < size; ++i)
+        // {
+        //     if (size / 2 > i)
+        //     {
+        //         s.push(curr->val);
+        //         curr = curr->next;
+        //     }
+        //     else
+        //     {
+        //         if (s.empty())
+        //         {
+        //             return false;
+        //         }
+        //         if (isOddCenter)
+        //         {
+        //             curr = curr->next;
+        //             isOddCenter = false;
+        //             continue;
+        //         }
+        //         if (s.top() == curr->val)
+        //         {
+        //             curr = curr->next;
+        //             s.pop();
+        //         }
+        //         else
+        //         {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
     }
 };
 // @lc code=end
