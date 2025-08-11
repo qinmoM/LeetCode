f908e0afb18bc290141ad69cdd2ff07c3346bb7a
qinmoM <ma200511@qq.com>
2025-08-11 16:18:47 +0800

    hash table : two sum

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -5,28 +5,42 @@
  */
 
 // @lc code=start
-#include <iostream>
+// #include <iostream>
+// #include <vector>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int, int> hash;
         for (int i = 0; i < nums.size(); i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            int need = target - nums[i];
+            if (hash.count(need))
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    return { i, j };
-                }
+                return { i, hash[need]};
             }
+            hash[nums[i]] = i;
         }
         return { };
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     for (int j = i + 1; j < nums.size(); j++)
+        //     {
+        //         if (nums[i] + nums[j] == target)
+        //         {
+        //             return { i, j };
+        //         }
+        //     }
+        // }
+        // return { };
     }
 };
-
 
 // @lc code=end
 
