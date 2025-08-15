9ac8166e8480053a50d0b7e8b33233514595c598
qinmoM <ma200511@qq.com>
2025-08-15 12:42:37 +0800

    hash table : find-the-difference

---
+++ 389.find-the-difference.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=389 lang=cpp
+ *
+ * [389] Find the Difference
+ */
+
+// @lc code=start
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    char findTheDifference(string s, string t)
+    {
+        std::unordered_map<char, int> hash;
+        for (char c : t)
+        {
+            ++hash[c];
+        }
+        for (char c : s)
+        {
+            --hash[c];
+        }
+        for (char c : t)
+        {
+            if (1 == hash[c])
+            {
+                return c;
+            }
+        }
+        throw -1;
+    }
+};
+// @lc code=end
+
