20c4d7bb520ed43a4ed481a59cfddafe08c897a4
qinmoM <ma200511@qq.com>
2025-08-21 23:57:45 +0800

    array optimize : greatest-common-divisor-of-strings

--- 1071.greatest-common-divisor-of-strings.cpp
+++ 1071.greatest-common-divisor-of-strings.cpp
@@ -6,6 +6,7 @@
 
 // @lc code=start
 #include <string>
+#include <numeric>
 
 using namespace std;
 
@@ -18,34 +19,37 @@ public:
         {
             return "";
         }
-        if (str1 == str2)
-        {
-            return str1;
-        }
-        if (str1.length() < str2.length())
-        {
-            swap(str1, str2);
-        }
 
-        int len = str1.length() - str2.length();
-        for (int i = len; i > 0; --i)
-        {
-            if (len % i)
-            {
-                continue;
-            }
-            string temp = str2.substr(0, i);
-            while (temp.length() < str1.length())
-            {
-                temp += str2.substr(0, i);
-            }
-            if (temp == str1)
-            {
-                return str2.substr(0, i);
-            }
-        }
+        return str1.substr(0, gcd(str1.length(), str2.length()));
 
-        return "";
+        // if (str1 == str2)
+        // {
+        //     return str1;
+        // }
+        // if (str1.length() < str2.length())
+        // {
+        //     swap(str1, str2);
+        // }
+
+        // int len = str1.length() - str2.length();
+        // for (int i = len; i > 0; --i)
+        // {
+        //     if (len % i)
+        //     {
+        //         continue;
+        //     }
+        //     string temp = str2.substr(0, i);
+        //     while (temp.length() < str1.length())
+        //     {
+        //         temp += str2.substr(0, i);
+        //     }
+        //     if (temp == str1)
+        //     {
+        //         return str2.substr(0, i);
+        //     }
+        // }
+
+        // return "";
     }
 };
 // @lc code=end
