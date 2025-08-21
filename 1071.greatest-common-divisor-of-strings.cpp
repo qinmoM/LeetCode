d811a8c374ca0bd567c451d77f52ad2edf00ed74
qinmoM <ma200511@qq.com>
2025-08-21 23:36:59 +0800

    array : greatest-common-divisor-of-strings

---
+++ 1071.greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=1071 lang=cpp
+ *
+ * [1071] Greatest Common Divisor of Strings
+ */
+
+// @lc code=start
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string gcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+        {
+            return "";
+        }
+        if (str1 == str2)
+        {
+            return str1;
+        }
+        if (str1.length() < str2.length())
+        {
+            swap(str1, str2);
+        }
+
+        int len = str1.length() - str2.length();
+        for (int i = len; i > 0; --i)
+        {
+            if (len % i)
+            {
+                continue;
+            }
+            string temp = str2.substr(0, i);
+            while (temp.length() < str1.length())
+            {
+                temp += str2.substr(0, i);
+            }
+            if (temp == str1)
+            {
+                return str2.substr(0, i);
+            }
+        }
+
+        return "";
+    }
+};
+// @lc code=end
+
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   
 ## Progress  
   
-_Last updata: 2025-08-20_  
+_Last updata: 2025-08-21_  
   
 ---
   
