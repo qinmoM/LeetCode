f19bd8ec6ecc976492eeb326533d39389b0092a2
qinmoM <ma200511@qq.com>
2025-08-26 12:36:31 +0800

    array : teemo-attacking

---
+++ 495.teemo-attacking.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=495 lang=cpp
+ *
+ * [495] Teemo Attacking
+ */
+
+// @lc code=start
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration)
+    {
+        int result = duration;
+        for (int i = 0; i < timeSeries.size() - 1; ++i)
+        {
+            int temp = timeSeries[i + 1] - timeSeries[i];
+            result += std::min(temp, duration);
+        }
+        return result;
+    }
+};
+// @lc code=end
+
