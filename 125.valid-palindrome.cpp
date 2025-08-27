4f992cea7998861c5f8d191b88efa78ed8be6c30
qinmoM <ma200511@qq.com>
2025-08-27 20:04:46 +0800

    string optimize : valid-palindrome

--- 125.valid-palindrome.cpp
+++ 125.valid-palindrome.cpp
@@ -14,29 +14,43 @@ class Solution
 public:
     bool isPalindrome(string s)
     {
+        int left = -1;
+        int right = s.length();
+        while (left < right)
         {
-            std::string temp;
-            for (char c : s)
-            {
-                if (std::isalnum(c))
-                {
-                    temp += tolower(c);
-                }
-            }
-            s.swap(temp);
-        }
-        
-        int left = 0;
-        int right = s.length() - 1;
-        while (right > left)
-        {
-            if (s[left++] != s[right--])
+            while (left < right && !std::isalnum(s[++left]));
+            while (left < right && !std::isalnum(s[--right]));
+
+            if (std::tolower(s[left]) != std::tolower(s[right]))
             {
                 return false;
             }
         }
-
         return true;
+
+        // {
+        //     std::string temp;
+        //     for (char c : s)
+        //     {
+        //         if (std::isalnum(c))
+        //         {
+        //             temp += tolower(c);
+        //         }
+        //     }
+        //     s.swap(temp);
+        // }
+        //
+        // int left = 0;
+        // int right = s.length() - 1;
+        // while (right > left)
+        // {
+        //     if (s[left++] != s[right--])
+        //     {
+        //         return false;
+        //     }
+        // }
+        //
+        // return true;
     }
 };
 // @lc code=end
