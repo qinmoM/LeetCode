e079dba8c374adc821c0852b0b52cb7d813d3a36
qinmoM <ma200511@qq.com>
2025-09-16 22:23:14 +0800

    tree : path-sum

---
+++ 112.path-sum.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=112 lang=cpp
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+class Solution
+{
+public:
+    bool hasPathSum(TreeNode* root, int targetSum)
+    {
+        if (nullptr == root)
+        {
+            return false;
+        }
+
+        if (nullptr == root->left && nullptr == root->right)
+        {
+            return targetSum == root->val;
+        }
+
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+
+};
+// @lc code=end
+
