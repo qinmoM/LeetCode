2a165e55a3f61cc74fa943c158394da9cb8594b4
qinmoM <ma200511@qq.com>
2025-10-16 12:10:25 +0800

    using backtracking : letter-combinations-of-a-phone-number

--- 17.letter-combinations-of-a-phone-number.cpp
+++ 17.letter-combinations-of-a-phone-number.cpp
@@ -17,26 +17,45 @@ public:
     vector<string> letterCombinations(string digits)
     {
         std::vector<std::string> hash = { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-        std::vector<std::string> temp;
         std::vector<std::string> result;
-        for (char c : hash[digits[0] - '2'])
-        {
-            result.push_back(std::string(1, c));
-        }
-        for (int i = 1; i < digits.length(); ++i)
-        {
-            int index = digits[i] - '2';
-            temp.clear();
-            for (char c : hash[index])
-            {
-                for (string& s : result)
-                {
-                    temp.push_back(s + std::string(1, c));
-                }
-            }
-            result.swap(temp);
-        }
+        combination(result, 0, "", hash, digits);
         return result;
+
+        // std::vector<std::string> hash = { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        // std::vector<std::string> temp;
+        // std::vector<std::string> result;
+        // for (char c : hash[digits[0] - '2'])
+        // {
+        //     result.push_back(std::string(1, c));
+        // }
+        // for (int i = 1; i < digits.length(); ++i)
+        // {
+        //     int index = digits[i] - '2';
+        //     temp.clear();
+        //     for (char c : hash[index])
+        //     {
+        //         for (string& s : result)
+        //         {
+        //             temp.push_back(s + std::string(1, c));
+        //         }
+        //     }
+        //     result.swap(temp);
+        // }
+        // return result;
+    }
+
+    void combination(std::vector<string>& result, int depth, std::string s, std::vector<std::string>& hash, std::string& target)
+    {
+        if (depth == target.length())
+        {
+            result.push_back(s);
+            return;
+        }
+
+        for (char c : hash[target[depth] - '2'])
+        {
+            combination(result, depth + 1, s + std::string(1, c), hash, target);
+        }
     }
 };
 // @lc code=end
