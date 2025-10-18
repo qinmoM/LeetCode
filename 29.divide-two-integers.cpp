6f1292aba154401564e20dec93bbe48d5be7d1ff
qinmoM_Linux <qinmo666@outlook.com>
2025-10-18 23:00:31 +0800

    using Bit Operation : divide-two-integers

--- 29.divide-two-integers.cpp
+++ 29.divide-two-integers.cpp
@@ -6,18 +6,56 @@
 
 // @lc code=start
 #include <iostream>
+#include <limits.h>
 
 class Solution
 {
 public:
     int divide(int dividend, int divisor)
     {
-        bool isNegative = ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0));
-        dividend = std::abs(dividend);
-        divisor = std::abs(divisor);
-        int level = 0;
-        while ((dividend -= divisor) >= 0) ++level;
-        return isNegative ? -level : level;
+        if (INT_MIN == dividend)
+        {
+            if (-1 == divisor)
+                return INT_MAX;
+            if (1 == divisor)
+                return INT_MIN;
+        }
+        bool isNegative = (dividend > 0) ^ (divisor > 0);
+        long long Dividend = std::abs(static_cast<long long>(dividend));
+        long long Divisor  = std::abs(static_cast<long long>(divisor));
+        long long result = 0;
+        while (Dividend >= Divisor)
+        {
+            long long tempDivisor = Divisor;
+            int level = 1;
+            while (Dividend >= (tempDivisor << 1))
+            {
+                tempDivisor <<= 1;
+                level <<= 1;
+            }
+            result += level;
+            Dividend -= tempDivisor;
+        }
+        if (isNegative)
+        {
+            result = -result;
+        }
+        if (INT_MAX <= result)
+        {
+            return INT_MAX;
+        }
+        if (INT_MIN >= result)
+        {
+            return INT_MIN;
+        }
+        return result;
+
+        // bool isNegative = ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0));
+        // dividend = std::abs(dividend);
+        // divisor = std::abs(divisor);
+        // int level = 0;
+        // while ((dividend -= divisor) >= 0) ++level;
+        // return isNegative ? -level : level;
     }
 };
 // @lc code=end
