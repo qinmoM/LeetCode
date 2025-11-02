ae07cd8b47b15e3ee446dfbfada6bd5f8c0fd6e6
qinmoM <ma200511@qq.com>
2025-11-02 16:40:05 +0800

    optimize projection : rectangle-overlap

--- 836.rectangle-overlap.cpp
+++ 836.rectangle-overlap.cpp
@@ -14,18 +14,20 @@ class Solution
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2)
     {
-        if (rec1[0] == rec2[0] && rec1[1] == rec2[1] && rec1[2] == rec2[2] && rec1[3] == rec2[3])
-        {
-            return true;
-        }
-        if (((rec1[0] > rec2[0] && rec1[0] < rec2[2]) || (rec1[2] > rec2[0] && rec1[2] < rec2[2]) ||
-             (rec2[0] > rec1[0] && rec2[0] < rec1[2]) || (rec2[2] > rec1[0] && rec2[2] < rec1[2]))&&
-            ((rec1[1] > rec2[1] && rec1[1] < rec2[3]) || (rec1[3] > rec2[1] && rec1[3] < rec2[3]) ||
-             (rec2[1] > rec1[1] && rec2[1] < rec1[3]) || (rec2[3] > rec1[1] && rec2[3] < rec1[3])) )
-        {
-            return true;
-        }
-        return false;
+        return !(rec1[0] >= rec2[2] || rec1[2] <= rec2[0] || rec1[1] >= rec2[3] || rec1[3] <= rec2[1]);
+
+        // if (rec1[0] == rec2[0] && rec1[1] == rec2[1] && rec1[2] == rec2[2] && rec1[3] == rec2[3])
+        // {
+        //     return true;
+        // }
+        // if (((rec1[0] > rec2[0] && rec1[0] < rec2[2]) || (rec1[2] > rec2[0] && rec1[2] < rec2[2]) ||
+        //      (rec2[0] > rec1[0] && rec2[0] < rec1[2]) || (rec2[2] > rec1[0] && rec2[2] < rec1[2]))&&
+        //     ((rec1[1] > rec2[1] && rec1[1] < rec2[3]) || (rec1[3] > rec2[1] && rec1[3] < rec2[3]) ||
+        //      (rec2[1] > rec1[1] && rec2[1] < rec1[3]) || (rec2[3] > rec1[1] && rec2[3] < rec1[3])) )
+        // {
+        //     return true;
+        // }
+        // return false;
 
         // if (rec1[0] == rec2[0] && rec1[1] == rec2[1] && rec1[2] == rec2[2] && rec1[3] == rec2[3])
         // {
