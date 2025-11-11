62c9d05abc420227b0e909d0267d2bb1fa0dc222
qinmoM <ma200511@qq.com>
2025-11-11 13:39:26 +0800

    using Math : vaild-boomerang

---
+++ 1037.valid-boomerang.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1037 lang=cpp
+ *
+ * [1037] Valid Boomerang
+ */
+
+// @lc code=start
+#include <vector>
+
+using std::vector;
+
+class Solution
+{
+public:
+    bool isBoomerang(vector<vector<int>>& points)
+    {
+        return !collineation(points) && !repetition(points);
+    }
+
+    bool repetition(std::vector<std::vector<int>>& points)
+    {
+        return (points[0][0] == points[1][0] && points[0][1] == points[1][1]) ||
+               (points[2][0] == points[1][0] && points[2][1] == points[1][1]) ||
+               (points[0][0] == points[2][0] && points[0][1] == points[2][1]);
+    }
+
+    bool collineation(std::vector<std::vector<int>>& points)
+    {
+        return (points[1][0] - points[0][0]) * (points[2][1] - points[0][1])
+            == (points[1][1] - points[0][1]) * (points[2][0] - points[0][0]);
+    }
+
+};
+// @lc code=end
+
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   
 ## Progress  
   
-_Last updata: 2025-11-10_  
+_Last updata: 2025-11-11_  
   
 ---
   
