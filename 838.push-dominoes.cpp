2aa44fc32f173839ae881f2116086021139df652
qinmoM <ma200511@qq.com>
2025-12-10 21:43:59 +0800

    Time Limit Exceeded

--- 838.push-dominoes.cpp
+++ 838.push-dominoes.cpp
@@ -6,8 +6,6 @@
 
 // @lc code=start
 #include <string>
-#include <vector>
-#include <algorithm>
 
 using std::string;
 
@@ -16,67 +14,106 @@ class Solution
 public:
     string pushDominoes(string dominoes)
     {
-        std::vector<int> vecL(dominoes.size(), 0);
-        std::vector<int> vecR(dominoes.size(), 0);
-        for (int i = 0; i < dominoes.size(); ++i)
+        if (1 >= dominoes.size()) return dominoes;
+        int size = dominoes.size();
+        int step = 1;
+        while (step)
         {
-            if (dominoes[i] == 'R') vecR[i] = 1;
-            else if (dominoes[i] == '.') vecR[i] = (0 != i ? vecR[i - 1] + 1 : 0);
-        }
-        for (int i = dominoes.size() - 1; i >= 0; --i)
-        {
-            if (dominoes[i] == 'L') vecL[i] = -1;
-            else if (dominoes[i] == '.') vecL[i] = (dominoes.size() - 1 != i ? vecL[i + 1] - 1 : 0);
-        }
-        auto temp = vecR.end();
-        for (auto it = vecR.begin(); it != vecR.end(); ++it)
-        {
-            if (0 != *(it))
+            step = 0;
+            std::string temp = dominoes;
+            if ('.' == dominoes[0] && 'L' == dominoes[1])
             {
-                if (vecR.end() == temp) temp = it;
+                temp[0] = 'L';
             }
-            else
+            for (int i = 1; i < size - 1; ++i)
             {
-                if (vecR.end() != temp)
+                if ('.' != dominoes[i]) continue;
+                if ('L' == dominoes[i + 1])
                 {
-                    std::reverse(temp, it);
-                    temp == vecR.end();
+                    if ('R' != dominoes[i - 1])
+                    {
+                        temp[i] = 'L';
+                        ++step;
+                    }
+                }
+                else
+                {
+                    if ('R' == dominoes[i - 1])
+                    {
+                        temp[i] = 'R';
+                        ++step;
+                    }
                 }
             }
-        }
-        if (vecR.end() != temp)
-        {
-            std::reverse(temp, vecR.end());
-            temp == vecR.end();
-        }
-        for (auto it = vecL.begin(); it != vecL.end(); ++it)
-        {
-            if (0 != *(it))
+            if ('.' == dominoes[size - 1] && 'R' == dominoes[size - 2])
             {
-                if (vecL.end() == temp) temp = it;
+                temp[size - 1] = 'R';
             }
-            else
-            {
-                if (vecL.end() != temp)
-                {
-                    std::reverse(temp, it);
-                    temp == vecL.end();
-                }
-            }
+            dominoes = temp;
         }
-        if (vecL.end() != temp)
-        {
-            std::reverse(temp, vecL.end());
-            temp == vecL.end();
-        }
-        for (int i = 0; i < vecR.size(); ++i) vecR[i] = vecR[i] + vecL[i];
-        std::string result(vecR.size(), '.');
-        for (int i = 0; i < vecR.size(); ++i)
-        {
-            if (vecR[i] > 0) result[i] = 'R';
-            else if (vecR[i] < 0) result[i] = 'L';
-        }
-        return result;
+        return dominoes;
+
+        // std::vector<int> vecL(dominoes.size(), 0);
+        // std::vector<int> vecR(dominoes.size(), 0);
+        // for (int i = 0; i < dominoes.size(); ++i)
+        // {
+        //     if (dominoes[i] == 'R') vecR[i] = 1;
+        //     else if (dominoes[i] == '.') vecR[i] = (0 != i ? vecR[i - 1] + 1 : 0);
+        // }
+        // for (int i = dominoes.size() - 1; i >= 0; --i)
+        // {
+        //     if (dominoes[i] == 'L') vecL[i] = -1;
+        //     else if (dominoes[i] == '.') vecL[i] = (dominoes.size() - 1 != i ? vecL[i + 1] - 1 : 0);
+        // }
+        // auto temp = vecR.end();
+        // for (auto it = vecR.begin(); it != vecR.end(); ++it)
+        // {
+        //     if (0 != *(it))
+        //     {
+        //         if (vecR.end() == temp) temp = it;
+        //     }
+        //     else
+        //     {
+        //         if (vecR.end() != temp)
+        //         {
+        //             std::reverse(temp, it);
+        //             temp == vecR.end();
+        //         }
+        //     }
+        // }
+        // if (vecR.end() != temp)
+        // {
+        //     std::reverse(temp, vecR.end());
+        //     temp == vecR.end();
+        // }
+        // for (auto it = vecL.begin(); it != vecL.end(); ++it)
+        // {
+        //     if (0 != *(it))
+        //     {
+        //         if (vecL.end() == temp) temp = it;
+        //     }
+        //     else
+        //     {
+        //         if (vecL.end() != temp)
+        //         {
+        //             std::reverse(temp, it);
+        //             temp == vecL.end();
+        //         }
+        //     }
+        // }
+        // if (vecL.end() != temp)
+        // {
+        //     std::reverse(temp, vecL.end());
+        //     temp == vecL.end();
+        // }
+        // for (int i = 0; i < vecR.size(); ++i) vecR[i] = vecR[i] + vecL[i];
+        // std::string result(vecR.size(), '.');
+        // for (int i = 0; i < vecR.size(); ++i)
+        // {
+        //     if (vecR[i] > 0) result[i] = 'R';
+        //     else if (vecR[i] < 0) result[i] = 'L';
+        // }
+        // return result;
     }
 };
 // @lc code=end
