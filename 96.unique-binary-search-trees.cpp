07056feea761f912239d5001a62ed1e6bcd56f19
qinmoM <ma200511@qq.com>
2025-12-20 14:41:17 +0800

    Optimize : unique-binary-search-trees

--- 96.unique-binary-search-trees.cpp
+++ 96.unique-binary-search-trees.cpp
@@ -5,22 +5,31 @@
  */
 
 // @lc code=start
+#include <unordered_map>
+
 class Solution
 {
 public:
     int numTrees(int n)
     {
-        return num(1, n);
+        std::unordered_map<int, int> hash;
+        return num(hash, 1, n);
     }
 
-    int num(int left, int right)
+    int num(std::unordered_map<int, int>& hash, int left, int right)
     {
         if (left >= right) return 1;
         int count = 0;
-        for (int i = left; i <= right; ++i)
+        int temp = right - left;
+        if (hash.find(temp) == hash.end())
         {
-            count += num(left, i - 1) * num(i + 1, right);
+            for (int i = left; i <= right; ++i)
+            {
+                count += num(hash, left, i - 1) * num(hash, i + 1, right);
+            }
+            hash[temp] = count;
         }
+        else count = hash[temp];
         return count;
     }
 };
