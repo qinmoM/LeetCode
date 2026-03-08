3299108394ea86efcf590a2ccdeabe4f50afc696
qinmoM <ma200511@qq.com>
2026-03-08 12:55:46 +0800

    using de-inorder : convert-bst-to-greater-tree

---
+++ 538.convert-bst-to-greater-tree.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=538 lang=cpp
+ *
+ * [538] Convert BST to Greater Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+class Solution
+{
+public:
+    TreeNode* convertBST(TreeNode* root)
+    {
+        int num = 0;
+        deinorder(root, num);
+        return root;
+    }
+
+    void deinorder(TreeNode* node, int& post)
+    {
+        if (!node) return;
+
+        deinorder(node->right, post);
+
+        post += node->val;
+        node->val = post;
+
+        deinorder(node->left, post);
+    }
+};
+// @lc code=end
+
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   
 ## Progress  
   
-_Last updata: 2025-03-07_  
+_Last updata: 2025-03-08_  
   
 ---
   
