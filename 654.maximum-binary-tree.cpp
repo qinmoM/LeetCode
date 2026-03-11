6227c4a3f22de43b31b6d95ed65f12d383cb7b54
qinmoM <ma200511@qq.com>
2026-03-12 00:49:27 +0800

    Optimize : maximum-binary-tree

--- 654.maximum-binary-tree.cpp
+++ 654.maximum-binary-tree.cpp
@@ -35,22 +35,42 @@ class Solution
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums)
     {
-        return recursion(nums, 0, nums.size());
-    }
+        std::vector<TreeNode*> vec;
 
-    TreeNode* recursion(const std::vector<int>& nums, int left, int right)
-    {
-        if (left >= right) return nullptr;
-
-        int maxIndex = left;
-        for (int i = left + 1; i < right; ++i)
+        for (int n : nums)
         {
-            if (nums[i] > nums[maxIndex])
-                maxIndex = i;
+            TreeNode* node = new TreeNode(n);
+
+            while (vec.size() && vec.back()->val < n)
+            {
+                node->left = vec.back();
+                vec.pop_back();
+            }
+
+            if (vec.size() && vec.back() != node)
+                vec.back()->right = node;
+
+            vec.push_back(node);
         }
 
-        return new TreeNode(nums[maxIndex], recursion(nums, left, maxIndex), recursion(nums, maxIndex + 1, right));
+        return vec.front();
+
+        // return recursion(nums, 0, nums.size());
     }
+
+    // TreeNode* recursion(const std::vector<int>& nums, int left, int right)
+    // {
+    //     if (left >= right) return nullptr;
+
+    //     int maxIndex = left;
+    //     for (int i = left + 1; i < right; ++i)
+    //     {
+    //         if (nums[i] > nums[maxIndex])
+    //             maxIndex = i;
+    //     }
+
+    //     return new TreeNode(nums[maxIndex], recursion(nums, left, maxIndex), recursion(nums, maxIndex + 1, right));
+    // }
 };
 // @lc code=end
 
