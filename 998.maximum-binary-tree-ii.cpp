b3bf069389b71f868baf44f07b3f6b5a6bd24a9e
qinmoM <ma200511@qq.com>
2026-03-30 13:42:26 +0800

    using DFS : maximum-binary-tree-ii

---
+++ 998.maximum-binary-tree-ii.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=998 lang=cpp
+ *
+ * [998] Maximum Binary Tree II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+class Solution
+{
+public:
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val)
+    {
+        if (!root || val > root->val)
+            return new TreeNode(val, root, nullptr);
+
+        root->right = insertIntoMaxTree(root->right, val);
+        return root;
+    }
+};
+// @lc code=end
+
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   
 ## Progress  
   
-_Last updata: 2025-03-29_  
+_Last updata: 2025-03-30_  
   
 ---
   
