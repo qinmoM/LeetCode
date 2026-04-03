0dbcc9c319e41c23a661f0c47c6bad6c136e23ee
qinmoM <ma200511@qq.com>
2026-04-03 18:56:44 +0800

    Optimize : two-city-scheduling

--- 1029.two-city-scheduling.cpp
+++ 1029.two-city-scheduling.cpp
@@ -5,8 +5,9 @@
  */
 
 // @lc code=start
-#include <map>
+// #include <map>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
@@ -15,23 +16,40 @@ class Solution
 public:
     int twoCitySchedCost(vector<vector<int>>& costs)
     {
-        std::multimap<int, std::vector<int>> map;
-        for (const std::vector<int>& vec : costs)
-            map.insert({vec[1] - vec[0], vec});
+        std::sort(costs.begin(), costs.end(),
+            [](const std::vector<int>& a, const std::vector<int>& b)
+            {
+                return a[0] - a[1] < b[0] - b[1];
+            }
+        );
 
-        int all = 0;
+        int res = 0;
         for (int i = 0; i < costs.size(); ++i)
         {
-            auto it = map.begin();
-
             if (i < costs.size() / 2)
-                all += it->second[1];
+                res += costs[i][0];
             else
-                all += it->second[0];
-
-            map.erase(it);
+                res += costs[i][1];
         }
-        return all;
+        return res;
+
+        // std::multimap<int, std::vector<int>> map;
+        // for (const std::vector<int>& vec : costs)
+        //     map.insert({vec[1] - vec[0], vec});
+        //
+        // int all = 0;
+        // for (int i = 0; i < costs.size(); ++i)
+        // {
+        //     auto it = map.begin();
+        //
+        //     if (i < costs.size() / 2)
+        //         all += it->second[1];
+        //     else
+        //         all += it->second[0];
+        //
+        //     map.erase(it);
+        // }
+        // return all;
     }
 };
 // @lc code=end
