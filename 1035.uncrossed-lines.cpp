3081bb88e01f4d89bfca2cac8c8f7c629574a8f6
qinmoM <ma200511@qq.com>
2026-04-07 15:10:17 +0800

    using Dynamic Programming : uncrossed-lines

--- 1035.uncrossed-lines.cpp
+++ 1035.uncrossed-lines.cpp
@@ -6,7 +6,6 @@
 
 // @lc code=start
 #include <vector>
-#include <unordered_map>
 
 using std::vector;
 
@@ -15,33 +14,46 @@ class Solution
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2)
     {
-        std::unordered_map<int, std::vector<int>> hash;
-        for (int i = 0; i < nums2.size(); ++i)
-            hash[nums2[i]].push_back(i);
-
-        int result = 0;
-        dfs(result, 0, 0, 0, nums1, hash);
-        return result;
-    }
-
-    void dfs(int& res, int temp, int curr1, int curr2, const vector<int>& nums1, const std::unordered_map<int, std::vector<int>> hash)
-    {
-        res = std::max(res, temp);
-        if (curr1 >= nums1.size()) return;
-
-        for (int i = curr1; i < nums1.size(); ++i)
+        std::vector<std::vector<int>> dp(nums1.size() + 1, std::vector<int>(nums2.size() + 1, 0));
+        for (int i = 1; i <= nums1.size(); ++i)
         {
-            auto it = hash.find(nums1[i]);
-            if (it == hash.end()) continue;
-            for (int j : it->second)
+            for (int j = 1; j <= nums2.size(); ++j)
             {
-                if (j < curr2)
-                    continue;
-
-                dfs(res, temp + 1, i + 1, j + 1, nums1, hash);
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
+        return dp[nums1.size()][nums2.size()];
+
+        // std::unordered_map<int, std::vector<int>> hash;
+        // for (int i = 0; i < nums2.size(); ++i)
+        //     hash[nums2[i]].push_back(i);
+        //
+        // int result = 0;
+        // dfs(result, 0, 0, 0, nums1, hash);
+        // return result;
     }
+
+    // void dfs(int& res, int temp, int curr1, int curr2, const vector<int>& nums1, const std::unordered_map<int, std::vector<int>> hash)
+    // {
+    //     res = std::max(res, temp);
+    //     if (curr1 >= nums1.size()) return;
+    //
+    //     for (int i = curr1; i < nums1.size(); ++i)
+    //     {
+    //         auto it = hash.find(nums1[i]);
+    //         if (it == hash.end()) continue;
+    //         for (int j : it->second)
+    //         {
+    //             if (j < curr2)
+    //                 continue;
+    //
+    //             dfs(res, temp + 1, i + 1, j + 1, nums1, hash);
+    //         }
+    //     }
+    // }
 };
 // @lc code=end
 
