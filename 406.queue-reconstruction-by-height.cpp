76a128c07a68ebf585075ebeb7ec11b5ff507ff8
qinmoM <ma200511@qq.com>
2026-04-29 12:08:38 +0800

    using vector.insert : queue-reconstruction-by-height

--- 406.queue-reconstruction-by-height.cpp
+++ 406.queue-reconstruction-by-height.cpp
@@ -17,35 +17,48 @@ class Solution
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people)
     {
-        std::map<int, std::set<int>> tree;
-
-        for (std::vector<int>& vec : people)
-        {
-            tree[vec[0]].insert(vec[1]);
-            vec[0] = -1;
-        }
-
-        for (const std::pair<int, std::set<int>>& set : tree)
-        {
-            int temp = 0;
-            auto it = set.second.begin();
-
-            for (int i = 0; i < people.size() && it != set.second.end(); ++i)
+        std::sort(people.begin(), people.end(),
+            [](const std::vector<int>& a, const std::vector<int>& b) -> bool
             {
-                if (-1 != people[i][0]) continue;
-
-                if (temp == *it)
-                {
-                    people[i][0] = set.first;
-                    people[i][1] = *it;
-                    ++it;
-                }
-
-                ++temp;
+                return a[0] > b[0] || (a[0] == b[0] && a[1] < b[1]);
             }
-        }
+        );
 
-        return people;
+        std::vector<std::vector<int>> res;
+        for (std::vector<int>& vec : people)
+            res.insert(res.begin() + vec[1], vec);
+
+        return res;
+
+        // std::map<int, std::set<int>> tree;
+        //
+        // for (std::vector<int>& vec : people)
+        // {
+        //     tree[vec[0]].insert(vec[1]);
+        //     vec[0] = -1;
+        // }
+        //
+        // for (const std::pair<int, std::set<int>>& set : tree)
+        // {
+        //     int temp = 0;
+        //     auto it = set.second.begin();
+        //
+        //     for (int i = 0; i < people.size() && it != set.second.end(); ++i)
+        //     {
+        //         if (-1 != people[i][0]) continue;
+        //
+        //         if (temp == *it)
+        //         {
+        //             people[i][0] = set.first;
+        //             people[i][1] = *it;
+        //             ++it;
+        //         }
+        //
+        //         ++temp;
+        //     }
+        // }
+        //
+        // return people;
     }
 };
 // @lc code=end
